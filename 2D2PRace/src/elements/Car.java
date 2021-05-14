638d7b6ba03e7a91adcc69cb5a3ce48cfa908bf3
Alexander Lan <alan285@student.fuhsd.org>
2021-05-14 10:32:06 -0700

    Updated Car

--- 2D2PRace/src/elements/Car.java
+++ 2D2PRace/src/elements/Car.java
@@ -8,14 +8,38 @@ import processing.core.PImage;
  * @author Alex Lan
  * @version 5/7/21
  */
-public class Car {
+public class Car extends MovingImage {
 	
-	private double xVelocity, yVelocity;
+	private static final int CAR_WIDTH = 45;
+	private static final int CAR_HEIGHT = 60;
+	
+	
+	private double xSpeed, ySpeed;
 	private Projectile projectile;
 	
-	public Car(PImage img) {
-		xVelocity = 0;
-		yVelocity = 0;
+	public Car(PImage img, int x, int y) {
+		super(img, x, y, CAR_WIDTH, CAR_HEIGHT);
+		xSpeed = 0;
+		ySpeed = 0;
+		projectile = new Projectile();
 	}
+	
+	public void accelerate() {
+		
+	}
+	
+	public void turn() {
+		
+	}
+	
+	public void boost() {
+		
+	}
+	
+	public void shoot() {
+		
+	}
+	
+	
 
 }
