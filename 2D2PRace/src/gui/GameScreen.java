8db256e13ce49c703b973b8cd50d4514812501d7
Connor Chow <legoman888@icloud.com>
2021-05-14 14:11:06 -0700

    Added 2 car images that are currently displayed

--- 2D2PRace/src/gui/GameScreen.java
+++ 2D2PRace/src/gui/GameScreen.java
@@ -2,6 +2,8 @@ package gui;
 
 import java.awt.event.KeyEvent;
 
+import elements.Car;
+
 /**
  * Used by DrawingSurface to draw the specified screen, which is to show the 
  * player the game interface
@@ -12,13 +14,21 @@ import java.awt.event.KeyEvent;
 public class GameScreen extends Screen {
 
 	private DrawingSurface surface;
+	private Car p1,p2;
 	
 	public GameScreen(DrawingSurface surface)
 	{
 		super();
 		this.surface = surface;
-		
-		
+	}
+	
+	/**
+	 * 
+	 */
+	public void setup()
+	{
+		p1 = new Car(surface.loadImage("car1-1.png"),560,350);
+		p2 = new Car(surface.loadImage("car1-2.png"),500,350);
 	}
 	
 	/**
@@ -26,12 +36,16 @@ public class GameScreen extends Screen {
 	 */
 	public void draw()
 	{
-//		if (surface.isPressed(KeyEvent.VK_LEFT))
-//			mario.walk(-1);
-//		if (surface.isPressed(KeyEvent.VK_RIGHT))
-//			mario.walk(1);
-//		if (surface.isPressed(KeyEvent.VK_UP))
-//			mario.jump();
+		surface.background(255);
+		p1.draw(surface);
+		p2.draw(surface);
+		
+		if (surface.isPressed(KeyEvent.VK_LEFT))
+			p2.turn();
+		if (surface.isPressed(KeyEvent.VK_RIGHT))
+			p2.turn();
+		if (surface.isPressed(KeyEvent.VK_UP))
+			p2.accelerate();
 		
 	}
 	
