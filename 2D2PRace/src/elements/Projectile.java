1c3b17f0ab0d0172554c9ba87119dbef74b60630
Connor Chow <legoman888@icloud.com>
2021-05-14 17:48:31 -0700

    projectile class

--- 2D2PRace/src/elements/Projectile.java
+++ 2D2PRace/src/elements/Projectile.java
@@ -1,16 +1,42 @@
 package elements;
+
+import processing.core.PImage;
+
 /**
  * Used to create the projectile used by the car to sabotage the other car during the game
  * 
  * @author Alex Lan
  * @version 5/7/21
  */
-public class Projectile {
+public class Projectile extends MovingImage{
 	
+	public static final int PROJ_WIDTH = 5;
+	public static final int PROJ_HEIGHT = 5;
 	
+	private double xv, yv;
+	private int lifespan;
+	
+	public Projectile(PImage img, int x, int y, int xv, int yv) {
+		super(img, x, y, PROJ_WIDTH, PROJ_HEIGHT);
+		
+		this.xv = xv;
+		this.yv = yv;
+		lifespan = 60;
+	}
 	
 	public void shoot() {
 		
 	}
+	
+	public void act() {
+		if(lifespan < 1)
+		{
+//			removeSelf(this);
+		}
+		
+		moveToLocation(getX()+xv,getY()+yv);
+		lifespan--;
+	}
+	
 
 }
