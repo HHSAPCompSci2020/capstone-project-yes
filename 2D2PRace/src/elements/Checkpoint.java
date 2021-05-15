e8d35dae547adc9c8ce6b6e2c7f47df2119262bf
Alexander Lan <alan285@student.fuhsd.org>
2021-05-14 17:27:02 -0700

    Edited Checkpoint

--- 2D2PRace/src/elements/Checkpoint.java
+++ 2D2PRace/src/elements/Checkpoint.java
@@ -1,4 +1,7 @@
 package elements;
+
+import processing.core.PApplet;
+
 /**
  * Used to create the checkpoint the car needs to pass during the game to prevent cheating
  * 
@@ -6,5 +9,22 @@ package elements;
  * @version 5/7/21
  */
 public class Checkpoint {
+	private static final int WIDTH = 45;
+	private static final int HEIGHT = 60;
+	
+	private int x;
+	private int y;
+	
+	public Checkpoint(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public void draw(PApplet surface) {
+		surface.fill(0, 0, 255);
+		surface.rect(x, y, WIDTH, HEIGHT);
+	}
+	
+	
 
 }
